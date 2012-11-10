bdf723c92d56f2d679441722dc26bf494915ed26
Simon Strom <simon.strom@gmail.com>
2012-11-10 15:03:17 +0100

    defender with shooting area

--- team25/Defender.java
+++ team25/Defender.java
@@ -5,9 +5,17 @@ import hockey.api.*;
 public class Defender extends BasePlayer {
     // Number of defender
     public int getNumber() { return getIndex() == 1 ? 1 : 2; }
+    
+    Position SHOOTING_POSITION = new Position(1000, 000);
 
     // Name of defender
-    public String getName() { return "Defender"; }
+    public String getName() { 
+      if (getIndex() == 1) {
+        return "Birgitta";
+      } else {
+       return "Gööörgy"; 
+      }
+    }
 
     // Make left defender left handed, right defender right handed.
     public boolean isLeftHanded() { return getIndex() == 1; }
@@ -15,6 +23,7 @@ public class Defender extends BasePlayer {
     // Initiate
     public void init() {
       setAimOnStick(false);
+      SHOOTING_POSITION = new Position(1000, normalY());
     }
 
     private int normalY() {
@@ -38,11 +47,30 @@ public class Defender extends BasePlayer {
         if (holder.isOpponent())
           skate(puck.getHolder(), MAX_SPEED);
         else
-          skate(puck.getX(), normalY(), MAX_SPEED);
-      } else if (puck.getX() < -1000) {
-        skate(puck, MAX_SPEED);
+          if (puckInForwardArea(puck)) {
+            moveToShootingArea();
+          } else {
+            skate(puck.getX(), normalY(), MAX_SPEED);
+          }
       } else {
-        skate(-2000, normalY(), 1000);
+        if (puck.getX() < -1000) {
+          skate(puck, MAX_SPEED);
+        } else if(puckInForwardArea(puck)) {
+          moveToShootingArea();
+        } else {
+          skate(-2000, normalY(), 1000);
+        }
       }
+    }
+
+    private void moveToShootingArea() {
+      skate(SHOOTING_POSITION, MAX_SPEED);
+    }
+
+    private boolean puckInForwardArea(IPuck puck) {
+      if (puck.getX() > 867) {
+        return true;
+      } 
+      return false;
     }
 }
