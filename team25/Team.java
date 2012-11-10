35e4fbd205851cb7bcab06dedef37a42318d8fa4
Simon Strom <simon.strom@gmail.com>
2012-11-10 11:48:47 +0100

    change team color

--- team25/Team.java
+++ team25/Team.java
@@ -13,10 +13,10 @@ public class Team implements ITeam {
     public String getTeamName() { return "Team Template"; }
 
     // Team color; body color
-    public Color getTeamColor() { return Color.MAGENTA; }
+    public Color getTeamColor() { return Color.RED; }
 
     // Team color; helmet color.
-    public Color getSecondaryTeamColor() { return Color.ORANGE; }
+    public Color getSecondaryTeamColor() { return Color.BLACK; }
 
     // The team's LUCKY NUMBER!!
     public int getLuckyNumber() { return 0; }
